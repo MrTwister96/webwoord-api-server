9cb29d2bd5cf785841ba60b2f26409241e5950ff
Schalk <schalkolivier@vegamine.com>
2021-10-11 21:27:50 +0200

    Updated Server

--- server.js
+++ server.js
@@ -2,11 +2,40 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { AccessToken } from "livekit-server-sdk";
+import { RoomServiceClient, Room } from "livekit-server-sdk";
 
 const app = express();
 const server = createServer(app);
 
 app.use(cors());
+
+const LKAPIKEY = "APIn26mHVrdLxBo";
+const LKAPISECRET = "MZmJfu0DecJ23eJv7azV1iAfyerPK7LsDXpPq7f6zCdI";
+
+app.get("/api/get-token", (req, res) => {
+    const roomName = req.query.roomName;
+    const identity = req.query.identity;
+    const host = req.query.host;
+
+    const canPublish = host ? true : false;
+
+    const accessParams = {
+        identity: identity,
+    };
+
+    const at = new AccessToken(LKAPIKEY, LKAPISECRET, accessParams);
+    at.addGrant({
+        roomJoin: true,
+        room: roomName,
+        canPublish: canPublish,
+        canSubscribe: !canPublish,
+    });
+
+    const token = at.toJwt();
+
+    return res.send({ token: token });
+});
 
 // Start Socket.IO
 const io = new Server(server, {
@@ -16,43 +45,31 @@ const io = new Server(server, {
     },
 });
 
-const streams = [
-    {
-        streamer: "EG Kerk Maranata",
-        speaker: "Ds Willie Olivier",
-        description: "Die Nagmal - 1 Kor 11:27",
-        listeners: 39,
-    },
-    {
-        streamer: "EG Kerk Pretoria Oos",
-        speaker: "Ds Andre Bester",
-        description: "Verbly jouself - Fil 4:4",
-        listeners: 27,
-    },
-    {
-        streamer: "EG Kerk Benoni",
-        speaker: "Ds Gerhard Beukes",
-        description: "Die Wonderlike Skepping - Gen 1",
-        listeners: 32,
-    },
-    {
-        streamer: "EG Kerk Kempton Park",
-        speaker: "Ds Paul Grobbelaar",
-        description: "Loof die Here - Ps 102",
-        listeners: 18,
-    },
-    {
-        streamer: "EG Kerk Wonderboom",
-        speaker: "Ds Jurgens Prinsloo",
-        description: "Die Here is my Herder - Ps 23",
-        listeners: 22,
-    },
-];
+let streams = [];
 
 io.on("connection", (socket) => {
-    console.log(`${socket.id} Connected`);
+    const livekitHost = "http://192.168.0.114:7880";
+    const svc = new RoomServiceClient(livekitHost, LKAPIKEY, LKAPISECRET);
+
+    // list rooms
+    svc.listRooms().then((rooms) => {
+        rooms.map((room) => {
+            const stream = {
+                streamer: room.name,
+                speaker: "Placeholder",
+                description: "Placeholder",
+                listeners: "Placeholder",
+            };
+
+            const newStreams = streams.filter((s) => s.streamer !== room.name);
+            streams = [...newStreams, stream];
+        });
+        console.log("existing rooms", rooms);
+    });
 
     socket.emit("all-streams", streams);
+
+    console.log(`${socket.id} Connected`);
 
     socket.on("disconnect", (reason) => {
         console.log(`${socket.id} Disconnected. Reason: ${reason}`);
@@ -60,7 +77,7 @@ io.on("connection", (socket) => {
 });
 
 // Start HTTP Server
-const PORT = process.env.EXPRESS_PORT || 5000;
+const PORT = process.env.EXPRESS_PORT || 5001;
 server.listen(PORT, () => {
     console.log(`SERVER STARTED ON ${PORT}`);
 });
